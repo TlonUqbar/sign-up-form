b8b5a05901b9dd19fd65ff8cbd4748f92c19a309
TlonUqbar <{125710315}+{TlonUqbar}@users.noreply.github.com>
2023-10-16 01:09:47 -0700

    feat:  Add complete sign up form with styles

---
+++ script.js
@@ -0,0 +1,32 @@
+const password1 = document.querySelector("#password");
+const password2 =  document.querySelector("#confirm");
+const error_msg = document.querySelector(".error-message");
+const success_msg = document.querySelector(".success-message");
+
+let p1 = '';
+let p2 = '';
+
+
+const comparePasswords = (e) => {
+  p1 = password1.value;
+  p2 = password2.value;
+  console.log(e);
+  if(p1 === p2){
+    console.log("true");
+    password1.classList = ["success"];
+    password2.classList = ["success"];
+    error_msg.style.display = "none";
+    success_msg.style.display = "block";
+  } else {
+    console.log("false");
+    password1.classList = ["error"];
+    password2.classList = ["error"];
+    error_msg.style.display = "block";
+    success_msg.style.display = 'none';
+  }
+
+}
+
+
+password1.addEventListener('input', comparePasswords);
+password2.addEventListener('input', comparePasswords);
